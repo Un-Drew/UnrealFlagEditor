7e3221c1fc0713e89a806e18904b04cd6802dfed
UnDrew <bandrayy@gmail.com>
2024-05-24 16:27:16 +0300

    Removed unnecessary imports and references.

--- src/Common/Nodes/EdNode_UObject.cs
+++ src/Common/Nodes/EdNode_UObject.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Drawing;
 using UELib;
 using UELib.Core;
 using UELib.Engine;
--- src/Form/EditorForm.cs
+++ src/Form/EditorForm.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using UELib;
-using UELib.Core;
 
 namespace UnrealFlagEditor
 {
